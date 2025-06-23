3feec764bde0e58b6953b7513c99e7f543503190
anupmenon22 <81994355+anupmenon22@users.noreply.github.com>
2025-06-23 13:21:17 -0400

    Create HomeController.cs

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Web.Mvc;
+using SampleDotNetApp.Models;
+
+namespace SampleDotNetApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(new MessageModel());
+        }
+
+        // POST: Home
+        [HttpPost]
+        public ActionResult Index(MessageModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (string.IsNullOrEmpty(model.Name))
+                {
+                    model.Result = "Please enter your name.";
+                    model.IsSuccess = false;
+                }
+                else if (string.IsNullOrEmpty(model.Message))
+                {
+                    model.Result = "Please enter a message.";
+                    model.IsSuccess = false;
+                }
+                else
+                {
+                    model.Result = $"Hello {model.Name}! Your message '{model.Message}' was received at {DateTime.Now:yyyy-MM-dd HH:mm:ss}.";
+                    model.IsSuccess = true;
+                }
+                model.ShowResult = true;
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Clear()
+        {
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+    }
+}
